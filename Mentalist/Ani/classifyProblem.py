e41fb1c10fe4c8f4c2c7fa66d0f358aef5c36809
AniDas10 <ani2004@gmail.com>
2020-03-07 20:19:53 +0530

    question pool ready

--- Mentalist/Ani/classifyProblem.py
+++ Mentalist/Ani/classifyProblem.py
@@ -1,8 +1,3 @@
-intro = {
-    1: ["What is your age?", "What is your gender?", "How are you feeling today?", "Have u ever consulted a psychcologist before?"],
-    2: ["What do you feel like doing?", "Do you feel like you need to depend on substances?", "Are you under a lot of pressure?", 
-        "How long can you work for at a time?", "Do you have any motivation to do work?"]
-}
 answers = [["Meet up with friends", "Go out by yourself", "Stay at home alone", "Lie in bed all day"], 
                 [ "Not really", "Once in a while", "Yes, all the time"], 
                 [ "Not really", "Tolerable amount", "Yes, very high"], 
